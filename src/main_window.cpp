fa5ae471d654704059aed5a8960011e9e453204b
Briton Frank <britonfrank@gmail.com>
2017-11-06 08:46:30 -0800

    refactored the code.

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -11,92 +11,8 @@
 #include <QVector>
 #include <array>
 
-double refreshTimer = 30;
+double refreshTimer = 30; // used as a constant for both places the refresh rate is used
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-MainWindow::MainWindow(QWidget * parent)
-	: QMainWindow(parent)
-{
-	setupUi();
-    pauseRunButton->setChecked(false);
-    blueLineDisplay->setChecked(true);
-    greenLineDisplay->setChecked(true);
-    redLineDisplay->setChecked(true);
-
-    minY->setValue(-100);
-    maxY->setValue(100);
-    domain->setValue(10);
-    blueLineRange->setValue(5);
-    greenLineRange->setValue(5);
-    redLineRange->setValue(5);
-
-    double upperRanges[] = {5.0,5.0,5.0,100.0};
-    double lowerRanges[] = {-5.0,-5.0,-5.0,-100.0};
-
-
-    QCPAxis *blueLineAxis = customPlot->axisRect(0)->addAxis(QCPAxis::atRight);
-    blueLineAxis->setRange(lowerRanges[0], upperRanges[0]);
-    blueLineAxis->setVisible(false);
-    
-    QCPAxis *greenLineAxis = customPlot->axisRect(0)->addAxis(QCPAxis::atRight);
-    greenLineAxis->setRange(lowerRanges[1], upperRanges[1]);
-    greenLineAxis->setVisible(false);
-
-    QCPAxis *redLineAxis = customPlot->axisRect(0)->addAxis(QCPAxis::atRight);
-    redLineAxis->setRange(lowerRanges[2], upperRanges[2]);
-    redLineAxis->setVisible(false);
-
-    QList<QCPAxis*> axes = {blueLineAxis,greenLineAxis,redLineAxis,customPlot->yAxis};
-    
-    customPlot->addGraph(customPlot->xAxis2,blueLineAxis); // blue line
-    customPlot->graph(0)->setPen(QPen(Qt::blue));
-
-    customPlot->addGraph(customPlot->xAxis2,greenLineAxis); // green line
-    customPlot->graph(1)->setPen(QPen(Qt::green));
-
-    customPlot->addGraph(customPlot->xAxis2,redLineAxis); // red line
-    customPlot->graph(2)->setPen(QPen(Qt::red));
-
-    
-    customPlot->xAxis->setRange(0,1);
-    customPlot->xAxis->QCPAxis::setRangeReversed(true);
-    customPlot->yAxis->setRange(lowerRanges[3],upperRanges[3]);
-    customPlot->xAxis->setTickLabelType(QCPAxis::ltNumber);
-    customPlot->xAxis->setAutoTickStep(false);
-    customPlot->xAxis->setTickStep(1000);
-
-    // this is used to see the x-axis to see accurate time.
-
-    // customPlot->xAxis2->setVisible(true);
-    // customPlot->xAxis2->setTickLabelType(QCPAxis::ltDateTime);
-    // customPlot->xAxis2->setDateTimeFormat("hh:mm:ss");
-    // customPlot->xAxis2->setAutoTickStep(true);
-    // customPlot->xAxis2->setTickStep(2);
-
-    connect(blueLineRange, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
-    [=](double d){
-        blueLineAxis->setRangeUpper(d);
-        customPlot->replot();
-    });
-    connect(greenLineRange, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
-    [=](double d){
-        greenLineAxis->setRangeUpper(d);
-        customPlot->replot();
-    });
-    connect(redLineRange, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
-    [=](double d){
-        redLineAxis->setRangeUpper(d);
-        customPlot->replot();
-    });
-
-
-    connect(maxY, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
-    [=](double d) mutable {
-        for (int i = 0; i < axes.size(); ++i)
-        {
-            axes[i]->setRangeUpper((upperRanges[i])*(d/100));
-=======
 GraphWindow::GraphWindow(QMainWindow * parent)
     : QMainWindow(parent)
 {
@@ -108,19 +24,6 @@ GraphWindow::GraphWindow(QMainWindow * parent)
     allPlots[2].plotGraph->setPen(QPen(Qt::green));
     allPlots[3].plotGraph->setPen(QPen(Qt::red));
 
-=======
-GraphWindow::GraphWindow(QMainWindow * parent)
-    : QMainWindow(parent)
-{
-    setupUi(); 
-    setRanges();
-    
-    allPlots[0].plotGraph->setPen(QPen(Qt::yellow));
-    allPlots[1].plotGraph->setPen(QPen(Qt::blue));
-    allPlots[2].plotGraph->setPen(QPen(Qt::green));
-    allPlots[3].plotGraph->setPen(QPen(Qt::red));
-
->>>>>>> Stashed changes
     // lambda expression used to connect signal from maxY QDoubleSpinBox so that when it is changed
     // throughout the program the range of the positive y-axis is decreased to the percent displayed.
     connect(maxY, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
@@ -128,10 +31,6 @@ GraphWindow::GraphWindow(QMainWindow * parent)
         for (auto &x: allPlots)
         {
             x.plotAxis->setRangeUpper((x.plotRange->value())*(d/100));
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
         }
         customPlot->replot();
     });
@@ -140,21 +39,9 @@ GraphWindow::GraphWindow(QMainWindow * parent)
     // throughout the program the range of the negative y-axis is decreased to the percent displayed.
     connect(minY, static_cast<void(QDoubleSpinBox::*)(double)>(&QDoubleSpinBox::valueChanged),
     [=](double d) mutable {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-        for (int i = 0; i < axes.size(); ++i)
-        {
-            axes[i]->setRangeLower((lowerRanges[i])*(-d/100));
-=======
         for (auto &x: allPlots)
         {
             x.plotAxis->setRangeLower((x.plotRange->value())*(d/100));
->>>>>>> Stashed changes
-=======
-        for (auto &x: allPlots)
-        {
-            x.plotAxis->setRangeLower((x.plotRange->value())*(d/100));
->>>>>>> Stashed changes
         }
         customPlot->replot();
     });
@@ -166,21 +53,9 @@ GraphWindow::GraphWindow(QMainWindow * parent)
         remove_data_to_scroll();
     }); 
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    connect(blueLineDisplay, SIGNAL(clicked()), this, SLOT(set_line_visible()));
-    connect(greenLineDisplay, SIGNAL(clicked()), this, SLOT(set_line_visible()));
-    connect(redLineDisplay, SIGNAL(clicked()), this, SLOT(set_line_visible()));
+    // connect the signal from the dataTime to call realtimeDataSlot() using the refresh interval "refreshTimer"
+    // set as a constant to be used throughout the class
 
-    // setup a timer that repeatedly calls MainWindow::realtimeDataSlot:
-=======
-    // connect the signal from the dataTime to call realtimeDataSlot() using the refresh interval "refreshTimer"
-    // set as a constant to be used throughout the class
->>>>>>> Stashed changes
-=======
-    // connect the signal from the dataTime to call realtimeDataSlot() using the refresh interval "refreshTimer"
-    // set as a constant to be used throughout the class
->>>>>>> Stashed changes
     connect(&dataTimer, SIGNAL(timeout()), this, SLOT(realtimeDataSlot()));
     dataTimer.start(refreshTimer); // Interval 0 means to refresh as fast as possible
 }
@@ -237,39 +112,6 @@ void GraphWindow::setupUi()
     maxY->setValue(100);
     
     domain = new QSpinBox(centralWidget);
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    domain->setGeometry(QRect(436,481,32,20));
-    
-    blueLineRange = new QDoubleSpinBox(centralWidget);
-    blueLineRange->setGeometry(QRect(580,12,63,20));
-    blueLineRange->setDecimals(1);
-    blueLineRange->setSingleStep(0.5);
-    
-    greenLineRange = new QDoubleSpinBox(centralWidget);
-    greenLineRange->setGeometry(QRect(580,38,63,20));
-    greenLineRange->setDecimals(1);
-    greenLineRange->setSingleStep(0.5);
-    
-    redLineRange = new QDoubleSpinBox(centralWidget);
-    redLineRange->setGeometry(QRect(580,63,63,20));
-    redLineRange->setDecimals(1);
-    redLineRange->setSingleStep(0.5);
-    
-    blueLineDisplay = new QCheckBox(centralWidget);
-    blueLineDisplay->setGeometry(QRect(650,12,87,17));
-    blueLineDisplay->setCheckable(true);
-    
-    greenLineDisplay = new QCheckBox(centralWidget);
-    greenLineDisplay->setGeometry(QRect(650,38,87,17));
-    greenLineDisplay->setCheckable(true);
-    
-    redLineDisplay = new QCheckBox(centralWidget);
-    redLineDisplay->setGeometry(QRect(650,63,87,17));
-    redLineDisplay->setCheckable(true);
-
-    retranslateUi();	
-=======
     domain->setGeometry(QRect(456,481,42,20));
     domain->setValue(10); // initialized the graph to show 10 seconds of data
     
@@ -290,28 +132,6 @@ void GraphWindow::setupUi()
     allPlots[3].plotRange = new QDoubleSpinBox(centralWidget);
     allPlots[3].plotRange->setGeometry(QRect(580,63,63,20));
 
-=======
-    domain->setGeometry(QRect(456,481,42,20));
-    domain->setValue(10); // initialized the graph to show 10 seconds of data
-    
-    allPlots.push_front(yellowLine);
-    allPlots.push_front(blueLine);
-    allPlots.push_front(greenLine);
-    allPlots.push_front(redLine);
-
-    allPlots[0].plotRange = new QDoubleSpinBox(centralWidget);
-    allPlots[0].plotRange->setGeometry(QRect(580,88,63,20));
-    
-    allPlots[1].plotRange = new QDoubleSpinBox(centralWidget);
-    allPlots[1].plotRange->setGeometry(QRect(580,12,63,20));
-
-    allPlots[2].plotRange = new QDoubleSpinBox(centralWidget);
-    allPlots[2].plotRange->setGeometry(QRect(580,38,63,20));
-
-    allPlots[3].plotRange = new QDoubleSpinBox(centralWidget);
-    allPlots[3].plotRange->setGeometry(QRect(580,63,63,20));
-
->>>>>>> Stashed changes
     allPlots[0].plotDisplay = new QCheckBox("YELLOW", centralWidget);
     allPlots[0].plotDisplay->setGeometry(QRect(650,88,87,17));
 
@@ -335,10 +155,6 @@ void GraphWindow::setupUi()
     // customPlot->xAxis2->setTickLabelType(QCPAxis::ltNumber);
     // customPlot->xAxis2->setAutoTickStep(true);
     // customPlot->xAxis2->setTickStep(2);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     QMetaObject::connectSlotsByName(this);
 }
@@ -382,60 +198,6 @@ void GraphWindow::on_pauseRunButton_clicked()
     customPlot->replot();
 }
 
-
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-void MainWindow::set_line_range()
-{
-    customPlot->replot();
-}
-
-void MainWindow::set_line_visible()
-{
-    blueLineDisplay->isChecked() ? customPlot->graph(0)->setVisible(true) : customPlot->graph(0)->setVisible(false);
-    greenLineDisplay->isChecked() ? customPlot->graph(1)->setVisible(true) : customPlot->graph(1)->setVisible(false);
-    redLineDisplay->isChecked() ? customPlot->graph(2)->setVisible(true) : customPlot->graph(2)->setVisible(false);
-    customPlot->replot();
-}
-
-void MainWindow::change_upper_y_range()
-{
-    
-}
-
-void MainWindow::change_lower_y_range(double d)
-{
-    customPlot->yAxis2->setRangeLower(d);
-    customPlot->replot();
-=======
-void GraphWindow::set_line_visible()
-// sets the visibility of the plot both when the plot is running and when it is paused.
-{
-    for(auto &x: allPlots)
-    {
-        x.plotDisplay->isChecked() ? x.plotGraph->setVisible(true) : x.plotGraph->setVisible(false);
-        customPlot->replot();
-    }
-    
->>>>>>> Stashed changes
-}
-
-void GraphWindow::remove_data_to_scroll()
-// modifies the x-axis based on the domain value and removes data outside of visible range
-{
-    // remove data of lines that's outside visible range:
-<<<<<<< Updated upstream
-    customPlot->graph(0)->removeDataBefore(key-(domain->value()));
-    customPlot->graph(1)->removeDataBefore(key-(domain->value()));
-    customPlot->graph(2)->removeDataBefore(key-(domain->value()));
-=======
-    for(auto &x: allPlots)
-    {
-        x.plotGraph->removeDataBefore(key-(domain->value()));
-    }
->>>>>>> Stashed changes
-    
-=======
 void GraphWindow::set_line_visible()
 // sets the visibility of the plot both when the plot is running and when it is paused.
 {
@@ -456,7 +218,6 @@ void GraphWindow::remove_data_to_scroll()
         x.plotGraph->removeDataBefore(key-(domain->value()));
     }
     
->>>>>>> Stashed changes
     customPlot->xAxis->setRange(0, domain->value()*1000);
 
     // make key axis range scroll with the data at a rate of the time value obtained from QSpinBox
@@ -471,26 +232,6 @@ void GraphWindow::realtimeDataSlot()
 {
     key += (refreshTimer/1000);
     
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    double value0 = sin(key);  
-    double value1 = sin(key+1) + 2;  
-    double value2 = sin(key+2) + 4;  
-
-    set_line_visible();
-
-    customPlot->graph(0)->addData(key, value0);
-    customPlot->graph(1)->addData(key, value1);
-    customPlot->graph(2)->addData(key, value2);
-    
-    remove_data_to_scroll();
-}
-void MainWindow::setupPlayground(QCustomPlot * customPlot)
-{
-	  Q_UNUSED(customPlot)
-}
-
-=======
     // this list is only used for demonstrations
     QList<double> value = {(sin(key)), (sin(key+1)), (sin(key+2)), (sin(key+3))};
     
@@ -501,16 +242,3 @@ void MainWindow::setupPlayground(QCustomPlot * customPlot)
     
     remove_data_to_scroll();
 }
->>>>>>> Stashed changes
-=======
-    // this list is only used for demonstrations
-    QList<double> value = {(sin(key)), (sin(key+1)), (sin(key+2)), (sin(key+3))};
-    
-    for(int i=0; i<allPlots.size(); i++)
-    {
-        allPlots[i].plotGraph->addData(key, value[i]);
-    }
-    
-    remove_data_to_scroll();
-}
->>>>>>> Stashed changes
--- src/main_window.h
+++ src/main_window.h
@@ -1,26 +1,11 @@
 #include <QMainWindow>
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-=======
 #include <QList>
->>>>>>> Stashed changes
-=======
-#include <QList>
->>>>>>> Stashed changes
 #include "qcustomplot.h"
 
 class GraphWindow : public QMainWindow
 {
 	Q_OBJECT
 public:
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-	MainWindow(QWidget * parent = 0);
-	double lastPointKey;
-	double key;
-=======
-=======
->>>>>>> Stashed changes
 	GraphWindow(QMainWindow * parent = 0);
 
 	struct Plot
@@ -46,10 +31,6 @@ private slots:
 
 private:
 	double key = 0;
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 	QTimer dataTimer;
 	QWidget *centralWidget;
     QCustomPlot *customPlot;
@@ -62,32 +43,6 @@ private:
     QLabel *label2;
     QLabel *label3;
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-private slots:
-	void setupPlayground(QCustomPlot * customPlot);
-	void on_pauseRunButton_clicked();
-	void remove_data_to_scroll();
-	void realtimeDataSlot();
-	void setupUi();
-	void retranslateUi();
-	void set_line_range();
-	void set_line_visible();
-	void change_upper_y_range();
-	void change_lower_y_range(double);
 
-private:
-	QDoubleSpinBox *blueLineRange;
-    QDoubleSpinBox *greenLineRange;
-    QDoubleSpinBox *redLineRange;
-
-    QCheckBox *blueLineDisplay;
-    QCheckBox *greenLineDisplay;
-    QCheckBox *redLineDisplay;
-=======
     QList<Plot> allPlots;
->>>>>>> Stashed changes
-=======
-    QList<Plot> allPlots;
->>>>>>> Stashed changes
 };
