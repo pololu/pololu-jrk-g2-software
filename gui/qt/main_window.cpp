13da2b008e1716560a0f34c111ff104709b1a7fb
Briton (Pololu) <dev-briton@pololu.com>
2018-01-15 08:26:26 -0800

    gui: Added a line to the main_window closeEvent that will close the popout graph window if the main_window is closed.

--- gui/qt/main_window.cpp
+++ gui/qt/main_window.cpp
@@ -1620,6 +1620,7 @@ void main_window::closeEvent(QCloseEvent * event)
     // User canceled exit when prompted about settings that have not been applied.
     event->ignore();
   }
+  altw->deleteLater();
 }
 
 void main_window::on_device_name_value_linkActivated()
