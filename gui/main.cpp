23c7a4fa1d78c4a6eba5eae299dd63a91df8e8ec
David (Pololu) <dev-david@pololu.com>
2018-01-15 10:28:22 -0800

    Added a comment about DPI scaling.

--- gui/main.cpp
+++ gui/main.cpp
@@ -5,6 +5,9 @@
 
 int main(int argc, char ** argv)
 {
+  // TODO: Disabling DPI scaling is wrong or needs an explanation.
+  // We enabled scaling in the Tic software and it worked fine,
+  // so why can't we enable it here?
 #if QT_VERSION >= QT_VERSION_CHECK(5,6,0)
   QApplication::setAttribute(Qt::AA_DisableHighDpiScaling);
 #else
