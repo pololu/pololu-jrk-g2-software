626eeb27a4244f0db045fb9f8276e78daaadeb1f
David (Pololu) <dev-david@pololu.com>
2018-01-24 18:17:17 -0800

    input wizard: Set up the 'learn' page, which shows the input value and an empty progress bar currenly.

--- gui/qt/input_wizard.cpp
+++ gui/qt/input_wizard.cpp
@@ -1,25 +1,42 @@
 #include "input_wizard.h"
 
+#include <QIcon>
 #include <QLabel>
+#include <QProgressBar>
 #include <QVBoxLayout>
+
+#ifdef __APPLE__
+#define NEXT_BUTTON_TEXT tr("Continue")
+#define FINISH_BUTTON_TEXT tr("Done")
+#else
+#define NEXT_BUTTON_TEXT tr("Next")
+#define FINISH_BUTTON_TEXT tr("Finish")
+#endif
 
 input_wizard::input_wizard(QWidget * parent)
   : QWizard(parent)
 {
   setPage(INTRO, setup_intro_page());
+  setPage(LEARN, setup_learn_page());
 
   setWindowTitle(tr("Input Setup Wizard"));
   setWindowIcon(QIcon(":app_icon"));  // TODO: make sure this works
   setWindowFlags(windowFlags() & ~Qt::WindowContextHelpButtonHint);
 
-  // TODO: try using setSizeConstraint(QLayout::SetFixedSize) on the
-  // layout object instead
   setFixedSize(sizeHint());
 }
 
 void input_wizard::set_input(uint16_t value)
 {
   input = value;
+
+  input_value->setText(QString::number(value));
+}
+
+void input_wizard::set_progress_visible(bool visible)
+{
+  sampling_label->setVisible(visible);
+  sampling_progress->setVisible(visible);
 }
 
 QWizardPage * input_wizard::setup_intro_page()
@@ -46,4 +63,66 @@ QWizardPage * input_wizard::setup_intro_page()
 
   page->setLayout(layout);
   return page;
+}
+
+QWizardPage * input_wizard::setup_learn_page()
+{
+  QWizardPage * page = new QWizardPage();
+  QVBoxLayout * layout = new QVBoxLayout();
+
+  instruction_label = new QLabel();
+  instruction_label->setWordWrap(true);
+  instruction_label->setAlignment(Qt::AlignTop);
+  instruction_label->setMinimumHeight(fontMetrics().lineSpacing() * 2);
+  layout->addWidget(instruction_label);
+  layout->addSpacing(fontMetrics().height());
+
+  layout->addLayout(setup_input_layout());
+  layout->addSpacing(fontMetrics().height());
+
+  QLabel * next_label = new QLabel(
+    tr("When you click ") + NEXT_BUTTON_TEXT +
+    tr(", this wizard will sample the input values for one second.  "
+    "Please do not change the input while it is being sampled."));
+  next_label->setWordWrap(true);
+  layout->addWidget(next_label);
+  layout->addSpacing(fontMetrics().height());
+
+  sampling_label = new QLabel(tr("Sampling..."));
+  layout->addWidget(sampling_label);
+
+  sampling_progress = new QProgressBar();
+  sampling_progress->setMaximum(SAMPLE_COUNT);
+  sampling_progress->setTextVisible(false);
+  layout->addWidget(sampling_progress);
+
+  set_progress_visible(false);
+
+  layout->addStretch(1);
+
+  page->setLayout(layout);
+
+  return page;
+}
+
+QLayout * input_wizard::setup_input_layout()
+{
+  QHBoxLayout * layout = new QHBoxLayout();
+
+  QLabel * input_label = new QLabel(tr("Input:"));
+  layout->addWidget(input_label);
+
+  input_value = new QLabel();
+  layout->addWidget(input_value);
+
+  // TODO: display the human-readable input units too
+  // (after we add it to the main window)
+
+  layout->addStretch(1);
+
+  // Set a fixed size for performance.
+  input_value->setText(QString::number(4095));
+  input_value->setFixedSize(input_value->sizeHint());
+
+  return layout;
 }
--- gui/qt/input_wizard.h
+++ gui/qt/input_wizard.h
@@ -1,14 +1,17 @@
 #pragma once
 
-#include <QObject>
-#include <QIcon>
-#include <QWidget>
 #include <QWizard>
 #include <stdint.h>
+
+class QLabel;
+class QProgressBar;
 
 class input_wizard : public QWizard
 {
   Q_OBJECT
+
+  // 20 samples, one every 50 ms.  Total time is 1000 ms.
+  static const uint32_t SAMPLE_COUNT = 20;
 
   enum page_number { INTRO, LEARN, CONCLUSION };
   enum learn_step { NEUTRAL, MAX, MIN };
@@ -33,7 +36,17 @@ public slots:
   void set_input(uint16_t);
 
 private:
+  void set_progress_visible(bool visible);
+
   QWizardPage * setup_intro_page();
+  QWizardPage * setup_learn_page();
+  QLayout * setup_input_layout();
+
+  // Controls on the 'Learn' page
+  QLabel * instruction_label;
+  QLabel * sampling_label;
+  QProgressBar * sampling_progress;
+  QLabel * input_value;
 
   uint16_t input;
 };
