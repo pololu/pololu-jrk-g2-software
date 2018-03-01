aacafbca7cc467dbedef9571ee28689787a31392
Briton (Pololu) <dev-briton@pololu.com>
2018-03-01 15:33:25 -0800

    gui: Refactored nice_spin_box.

--- gui/qt/nice_spin_box.cpp
+++ gui/qt/nice_spin_box.cpp
@@ -3,19 +3,19 @@
 
 #include <QLineEdit>
 #include <QSpinBox>
-
+#include <QtMath>
+#include <QRegExpValidator>
+#include <QDebug>
 #include <iostream> //tmphax
 
 nice_spin_box::nice_spin_box(int index, QWidget* parent)
   : index(index), QDoubleSpinBox(parent)
 {
-  connect(this, QOverload<double>::of(&QDoubleSpinBox::valueChanged),
-    this, &set_code);
-  connect(this, &QDoubleSpinBox::editingFinished, this, &editing_finished);
+  connect(this, QOverload<const QString&>::of(&QDoubleSpinBox::valueChanged), this, &editing_finished);
 
+  setRange(-1, 10000);
   setKeyboardTracking(false);
-  setSuffix(" A");
-  setDecimals(3);
+  setDecimals(2);
 }
 
 void nice_spin_box::set_controller(main_controller * controller)
@@ -23,54 +23,63 @@ void nice_spin_box::set_controller(main_controller * controller)
   this->controller = controller;
 }
 
-void nice_spin_box::editing_finished()
+void nice_spin_box::editing_finished(const QString& text)
 {
   if (suppress_events) { return; }
-  for(int i = 0; i < mapping.size() - 1; i++)
+
+  qDebug() << text;
+
+  entered_value = text.toDouble();
+
+  double smallest_diff = qFabs(entered_value - mapping.values().at(0));
+
+  for(int i = 1; i < mapping.size() - 1; i++)
   {
-    if (value() >= mapping.at(i).second && value() < mapping.at(i + 1).second)
+    double current_diff = qFabs(entered_value - mapping.values().at(i));
+    if (current_diff < smallest_diff)
     {
-      current_index = mapping.at(i).first;
+      smallest_diff = current_diff;
+      int closest = i;
+      current_index = closest;
     }
   }
 
-  if (value() > mapping.last().second)
+  if (entered_value > mapping.values().last())
   {
     current_index = mapping.size() - 1;
   }
 
-  set_display_value();
+  set_code();
 }
 
 void nice_spin_box::set_display_value()
 {
   suppress_events = true;
-  setValue(mapping.at(current_index).second);
+  setValue(mapping[current_index]);
   suppress_events = false;
 }
 
 void nice_spin_box::set_possible_values(uint16_t value)
 {
   mapping.clear();
-  mapping.append(qMakePair(0, (controller->get_current_limit_value(0)/1000)));
+  mapping.insert(0, (controller->get_current_limit_value(0)/1000));
 
   for (int i = 1; i < 96; i++)
   {
     double display_value = (controller->get_current_limit_value(i)/1000);
 
-    mapping.append(qMakePair(i, display_value));
+    mapping.insert(i, display_value);
   }
 
   current_index = value;
 
   set_display_value();
-
 }
 
 void nice_spin_box::stepBy(int step_value)
 {
-  if (mapping.at(qBound(0, (current_index + step_value),
-    mapping.size() - 1)).second == value())
+  if (mapping.values().at(qBound(0, (current_index + step_value),
+    mapping.size() - 1)) == value())
   {
     current_index += (step_value * 2);
   }
@@ -79,7 +88,7 @@ void nice_spin_box::stepBy(int step_value)
 
   current_index = qBound(0, current_index, mapping.size() - 1);
 
-  setValue(mapping.at(current_index).second);
+  setValue(mapping[current_index]);
 }
 
 QDoubleSpinBox::StepEnabled nice_spin_box::stepEnabled()
@@ -99,5 +108,5 @@ QDoubleSpinBox::StepEnabled nice_spin_box::stepEnabled()
 void nice_spin_box::set_code()
 {
   if (suppress_events) { return; }
-  controller->handle_current_limit_amps_spinbox_input(index, mapping.at(current_index).first);
+  controller->handle_current_limit_amps_spinbox_input(index, current_index);
 }
--- gui/qt/nice_spin_box.h
+++ gui/qt/nice_spin_box.h
@@ -17,18 +17,20 @@ public:
   void set_possible_values(uint16_t value);
 
 private slots:
-  void editing_finished();
+  void editing_finished(const QString&);
   void set_display_value();
   void set_code();
 
 private:
   main_controller * controller;
-  QList<QPair<int, double>> mapping;
+  QMap<int, double> mapping;
   int current_index = 0;
   int index;
   bool suppress_events = false;
+  double entered_value = -1;
 
   // Reimplemented QDoubleSpinBox functions
   virtual void stepBy(int step_value);
   virtual StepEnabled stepEnabled();
 };
+
