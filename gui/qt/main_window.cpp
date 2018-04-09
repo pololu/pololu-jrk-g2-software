01685d8e5b54e40df5634583f5a9804a7bb75790
Briton (Pololu) <dev-briton@pololu.com>
2018-04-09 11:21:39 -0700

    gui: Added call to center_at_startup_if_needed and removed unnecessary comment.

--- gui/qt/main_window.cpp
+++ gui/qt/main_window.cpp
@@ -1021,12 +1021,7 @@ void main_window::center_at_startup_if_needed()
   //
   // In case this causes problems, you can set the JRK2GUI_CENTER environment
   // variable to "N".
-  //
-  // NOTE: This position issue on Raspbian is a bug in Qt that should be fixed.
-  // Another workaround for it was to uncomment the lines in retranslate() that
-  // set up errors_stopping_header_label, error_rows[*].name_label, and
-  // manual_target_velocity_mode_radio, but then the Window would strangely
-  // start in the lower right.
+
   auto env = QProcessEnvironment::systemEnvironment();
   if (env.value("JRK2GUI_CENTER") != "N")
   {
@@ -1046,6 +1041,7 @@ void main_window::showEvent(QShowEvent * event)
   if (!start_event_reported)
   {
     start_event_reported = true;
+    center_at_startup_if_needed();
     controller->start();
   }
 }
