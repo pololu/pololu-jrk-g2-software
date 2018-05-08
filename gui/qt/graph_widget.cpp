57e412b74219cd775efa4f706268ab6e7a145121
Briton (Pololu) <dev-briton@pololu.com>
2018-05-08 09:06:53 -0700

    gui: Changed unicode characters for graph_widget.

--- gui/qt/graph_widget.cpp
+++ gui/qt/graph_widget.cpp
@@ -243,7 +243,7 @@ void graph_widget::setup_plot(plot& plot, QString display_text, QString default_
   plot.display->setCheckable(true);
   plot.display->setChecked(default_visible);
 
-  plot.reset_button = new QPushButton("\u27f2");
+  plot.reset_button = new QPushButton("\u21ba");
   plot.reset_button->setStyleSheet("QPushButton{margin: 0px; padding: 2px;}");
   plot.reset_button->setToolTip("Reset " + display_text + " plot\nposition and scale");
 
@@ -267,7 +267,7 @@ void graph_widget::setup_plot(plot& plot, QString display_text, QString default_
   QSharedPointer<QCPAxisTickerText> plot_axis_ticker(new QCPAxisTickerText);
   plot_axis_ticker->setTickCount(0);
   plot_axis_ticker->setSubTickCount(0);
-  plot_axis_ticker->addTick(0, "\u2B9E");
+  plot_axis_ticker->addTick(0, "\u27a4");
   plot.axis->setTicker(plot_axis_ticker);
 
   QFont font;
